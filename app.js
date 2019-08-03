fc5fe34d1c5b17fef1318200c6915cd7a9f5dc6c
ngsw <ngsw@ngsw.jp>
2019-08-03 23:18:56 +0900

    port戻した

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get('/', function (req, res) {
 });
 
 // サーバーを起動する部分
-var server = app.listen(3003, function () {
+var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
